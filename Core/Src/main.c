59ae5d5f2901ed23b7cd8ea27d493c0f388d1efc
jyc001 <60777728+jyc001@users.noreply.github.com>
2023-02-10 19:33:53 +0800

    final

---
+++ Core/Src/main.c
@@ -0,0 +1,39 @@
+/********************************** (C) COPYRIGHT *******************************
+* File Name          : main.c
+* Author             : WCH
+* Version            : V1.0.0
+* Date               : 2021/06/06
+* Description        : Main program body.
+* Copyright (c) 2021 Nanjing Qinheng Microelectronics Co., Ltd.
+* SPDX-License-Identifier: Apache-2.0
+*******************************************************************************/
+
+
+
+#include "main.h"
+
+/* Global define */
+
+/* Global Variable */
+
+
+/*********************************************************************
+ * @fn      main
+ *
+ * @brief   Main program.
+ *
+ * @return  none
+ */
+int main(void) {
+
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+    Delay_Init();
+    USART_Printf_Init(115200);
+    printf("SystemClk: %d\r\n", SystemCoreClock);
+
+
+    while (1) {
+
+        printf("Test \r\n");
+    }
+}
